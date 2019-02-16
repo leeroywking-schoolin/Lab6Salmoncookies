5018f288b02023fa99853bb99aa66c758ad69bd0
Lee-Roy King <leeroywking@gmail.com>
2019-02-16 08:29:01 -0800

    got the site up and running per specifications

--- app.js
+++ app.js
@@ -1,67 +1,152 @@
 'use strict';
 
-var days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
+var hours = [
+  '6am',
+  '7am',
+  '8am',
+  '9am',
+  '10am',
+  '11am',
+  '12am',
+  '1pm',
+  '2pm',
+  '3pm',
+  '4pm',
+  '5pm',
+  '6pm',
+  '7pm',
+  '8pm'
 ];
 
-var alkiUl = document.getElementById('alkibeach');
-var goldenGardensUl = document.getElementById('gg');
-var edmondsUl = document.getElementById('edmondsbeach');
+var firstAndPike = document.getElementById('1andpike');
+var seaTac = document.getElementById('seatac');
+var seattleCenter = document.getElementById('seacenter');
+var caphill = document.getElementById('caphill');
+var alki = document.getElementById('alkibeach');
 
-var alki = {
-  name: 'Alki',
-  neighborhood: 'West Seattle',
-  seagullCount: [212, 44, 34, 14, 15, 23, 42],
+var firstAPObject = {
+  maxCustPerHour: 65,
+  minCustPerHour: 23,
+  cookiesPerSale: 6.3,
+  name: 'First and Pike',
+  randRange: function(){
+    return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)},
+  cookiesPerHour: this.numCustomerPerHour * this.cookiesPerSale,
+
   render: function () {
-    for (var i = 0; i < days.length; i++) {
+    var total = 0
+    for (var i = 0; i < hours.length; i++) {
       var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      alkiUl.appendChild(liEl);
+      var numCustomerPerHour = this.randRange();
+      var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
+      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      firstAndPike.appendChild(liEl);
+      total = cookiesPerHour + total;
     }
+    var liEltotal = document.createElement('li');
+    liEltotal.textContent = `Total: ${total} cookies.`;
+    firstAndPike.appendChild(liEltotal);
   }
 };
 
-var goldenGardens = {
-  name: 'Golden Gardens',
-  neighborhood: 'Crown Hill',
-  seagullCount: [
-    22,
-    440,
-    324,
-    5,
-    15,
-    3,
-    423
-  ],
+var seaTacObject = {
+  maxCustPerHour: 24,
+  minCustPerHour: 2,
+  cookiesPerSale: 1.2,
+  randRange: function(){
+    return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)},
+  cookiesPerHour: this.numCustomerPerHour * this.cookiesPerSale,
+
   render: function () {
-    for (var i = 0; i < days.length; i++) {
+    var total = 0
+    for (var i = 0; i < hours.length; i++) {
       var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      goldenGardensUl.appendChild(liEl);
+      var numCustomerPerHour = this.randRange();
+      var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
+      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      seaTac.appendChild(liEl);
+      total = cookiesPerHour + total;
     }
-  }
-}
-var edmondsBeach = {
-  name: 'Edmonds Beach',
-  neighborhood: 'North Seattle',
-  seagullCount: [12, 4, 3, 4, 5, 2, 4],
-  render: function () {
-    for (var i = 0; i < days.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      edmondsUl.appendChild(liEl);
-    }
+    var liEltotal = document.createElement('li');
+    liEltotal.textContent = `Total: ${total} cookies.`;
+    seaTac.appendChild(liEltotal);
   }
 };
 
+var seaCentObject = {
+  maxCustPerHour: 38,
+  minCustPerHour: 11,
+  cookiesPerSale: 3.7,
+  randRange: function(){
+    return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)},
+  cookiesPerHour: this.numCustomerPerHour * this.cookiesPerSale,
 
+  render: function () {
+    var total = 0
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      var numCustomerPerHour = this.randRange();
+      var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
+      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      seattleCenter.appendChild(liEl);
+      total = cookiesPerHour + total;
+    }
+    var liEltotal = document.createElement('li');
+    liEltotal.textContent = `Total: ${total} cookies.`;
+    seattleCenter.appendChild(liEltotal);
+  }
+};
 
-alki.render();
-goldenGardens.render();
-edmondsBeach.render();
+var capHillObject = {
+  maxCustPerHour: 38,
+  minCustPerHour: 20,
+  cookiesPerSale: 2.3,
+  randRange: function(){
+    return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)},
+  cookiesPerHour: this.numCustomerPerHour * this.cookiesPerSale,
+
+  render: function () {
+    var total = 0
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      var numCustomerPerHour = this.randRange();
+      var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
+      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      caphill.appendChild(liEl);
+      total = cookiesPerHour + total;
+    }
+    var liEltotal = document.createElement('li');
+    liEltotal.textContent = `Total: ${total} cookies.`;
+    caphill.appendChild(liEltotal);
+  }
+};
+
+var alkiBeachObject = {
+  maxCustPerHour: 16,
+  minCustPerHour: 2,
+  cookiesPerSale: 4.6,
+  randRange: function(){
+    return Math.floor((Math.random() *(this.maxCustPerHour - this.minCustPerHour)) + this.minCustPerHour)},
+  cookiesPerHour: this.numCustomerPerHour * this.cookiesPerSale,
+
+  render: function () {
+    var total = 0
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      var numCustomerPerHour = this.randRange();
+      var cookiesPerHour = Math.floor(numCustomerPerHour * this.cookiesPerSale);
+      liEl.textContent = `${hours[i]}: ${cookiesPerHour} cookies.`;
+      alki.appendChild(liEl);
+      total = cookiesPerHour + total;
+    }
+    var liEltotal = document.createElement('li');
+    liEltotal.textContent = `Total: ${total} cookies.`;
+    alki.appendChild(liEltotal);
+  }
+};
+
+firstAPObject.render();
+seaTacObject.render();
+seaCentObject.render();
+capHillObject.render();
+alkiBeachObject.render();
